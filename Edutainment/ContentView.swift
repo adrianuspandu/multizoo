a5e0322d9548557001ba59f94e4685b376a1bb5c
Adrianus Pandu Wicaksono <panduadrianus@gmail.com>
2025-04-09 16:40:12 +0200

    Initial Commit

---
+++ Edutainment/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Edutainment
+//
+//  Created by student on 09.04.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
